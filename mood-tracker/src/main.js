d83ed75c2c58732536ffe5c745e98bdf896ff8a3
jin-hyun <abyssul1@naver.com>
2023-12-20 21:50:52 +0900

    ✨ 알림 추가

--- mood-tracker/src/main.js
+++ mood-tracker/src/main.js
@@ -2,11 +2,12 @@ import { createApp } from "vue";
 import "tailwindcss/tailwind.css";
 import App from "./App.vue";
 import axios from "axios";
-import router from "./router";
+// import router from "./router";
+import routerN from "./router/notification";
 
 const app = createApp(App);
 
 app.config.globalProperties.$axios = axios; //전역변수로 설정 컴포넌트에서 this.$axios 호출할 수 있게 설정
 app.config.globalProperties.$serverUrl = "//localhost:8081"; //api server
 
-app.use(router).mount("#app");
+app.use(routerN).mount("#app");
