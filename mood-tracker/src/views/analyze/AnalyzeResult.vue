f2b45ec7b3d09d113b740cc87dec458b62465f1f
C-sopy <yeoneeso@naver.com>
2023-12-22 18:41:01 +0900

    💄 얼굴인식 화면 수정

--- mood-tracker/src/views/analyze/AnalyzeResult.vue
+++ mood-tracker/src/views/analyze/AnalyzeResult.vue
@@ -1,21 +1,45 @@
 <template>
-    <div class="flex h-screen">
-      <div class="w-1/5 bg-sky-200">side menu</div>
-      <div class="flex-1 ">
-      
-        <div class="w-full h-screen bg-[#E7F1E5]"></div>
+  <div class="flex h-screen">
+    <div class="w-1/5 p-4">side menu</div>
 
+    <div class="flex-1 w-full bg-[#E7F1E5]">
+      <!-- 여기서부터 ~~~ -->
+      <div class="flex justify-center items-center">
 
+        <div class="flex flex-col w-[700px] h-auto mt-5 bg-slate-200 items-center justify-center">
+          <!-- 감정 이모티콘 -->
+          <div class="flex w-[500px] h-64 bg-[#ffede6]" id="imoji">
+
+            <div class="flex">♡</div>
+            <div class="flex">
+              <span>오늘의 기분</span><br />
+              <span>아주 최고야</span>
+            </div>
+          </div>
+          <!-- radar chart -->
+          <div id="pentagone" class="flex w-[500px] h-64 bg-[#e39a7d]">
+            <div>차트</div>
+            <dic>내용</dic>
+          </div>
+
+        </div>
       </div>
-      <div class="w-1/5 bg-sky-200">side menu</div>
+
+      <!-- 여까지 -->
     </div>
-  </template>
+
+    <div class="w-1/5 p-4">side menu</div>
+  </div>
+</template>
   
-  <script>
-  export default {
-    name: "AnalyzeResult",
-  };
-  </script>
+<script>
+
+
+export default {
+  name: "AnalyzeResult",
   
-  <style scoped>
-  </style>
+};
+
+</script>
+  
+<style scoped></style>
