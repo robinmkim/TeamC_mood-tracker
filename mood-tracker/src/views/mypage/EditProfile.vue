e4da7254f4cb3b4d528550b855a8eb4935e5c2e4
kana0602 <kana0602@naver.com>
2023-12-22 19:24:02 +0900

    ✨ 나의페이지 편집과 프로필편집들어갈수 있는 버튼 추가 달력추가

---
+++ mood-tracker/src/views/mypage/EditProfile.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="flex min-h-screen">
+      <div class="w-1/5">
+        <side-bar> <h1> 안녕하세요 프로필 편집 부분입니다.ㅇ</h1></side-bar>
+      </div>
+      <div class="flex-1 border-x">
+        <home-post></home-post>
+      </div>
+      <div class="w-1/5">side menu</div>
+    </div>
+  </template>
