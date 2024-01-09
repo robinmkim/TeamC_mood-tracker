6e50532cc52f8a8bb4b6befb4ffab91398cf73f1
jin-hyun <abyssul1@naver.com>
2024-01-09 17:54:16 +0900

    👔  Board user info print

---
+++ mood-tracker-be/src/main/java/com/teamc/moodtracker/dto/JH_MemberDto.java
@@ -0,0 +1,19 @@
+package com.teamc.moodtracker.dto;
+
+import lombok.Getter;
+
+@Getter
+public class MemberDto {
+    private int m_id;
+    private String  m_name;
+    private String m_handle;
+    private String m_email;
+    private String m_pwd;
+    private String m_bdate;
+    private String regdate;
+    private String m_gender;
+    private String m_img_name;
+    private String m_img_path;
+    private String m_dio;
+
+}
