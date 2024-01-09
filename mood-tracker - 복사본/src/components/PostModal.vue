28cb0ec8040fd74a10661802099f8ba4f6aadbda
Lee-Giyoung <145593169+Lee-Giyoung@users.noreply.github.com>
2024-01-09 14:16:00 +0900

    ✨ FnQ Insert등록

---
+++ mood-tracker - 복사본/src/components/PostModal.vue
@@ -0,0 +1,150 @@
+<template>
+  <div
+    v-if="isOpen"
+    class="fixed inset-0 bg-black bg-opacity-50 overflow-y-auto h-full w-full flex justify-center items-center"
+  >
+    <div class="lg:w-10/12 h-2/3 flex flex-col">
+      <div class="ml-auto cursor-pointer mb-1" @click="closeModal">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          class="w-10 h-10"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M5.47 5.47a.75.75 0 0 1 1.06 0L12 10.94l5.47-5.47a.75.75 0 1 1 1.06 1.06L13.06 12l5.47 5.47a.75.75 0 1 1-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 0 1-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 0 1 0-1.06Z"
+            clip-rule="evenodd"
+          />
+        </svg>
+      </div>
+      <div class="h-5/6 bg-white rounded shadow-lg flex overflow-hidden">
+        <div class="flex flex-col min-h-5/6 flex-grow overflow-auto">
+          <div class="flex h-14 border-b justify-center items-center">
+            12월 27일
+          </div>
+          <div class="flex h-16 border-b items-center justify-start pl-4">
+            <span>😀 강아지 너무 좋아</span>
+          </div>
+          <div class="flex h-16 border-b items-center justify-start pl-4">
+            <span>😀 강아지 너무 좋아</span>
+          </div>
+        </div>
+        <div class="border-x flex flex-grow">
+          <div class="h-full">
+            <!-- post 내용 -->
+            <postDetail></postDetail>
+          </div>
+          <div class="flex flex-col flex-grow border-l border-l-slate-300">
+            <!-- 댓글창 -->
+            <div class="p-3 border-b border-b-slate-300">
+              <div class="postHerder flex flex-row m">
+                <div class="h-14 w-14 overflow-hidden relative">
+                  <img
+                    class="postDetailUserImg object-contain rounded-full"
+                    src="@/assets/logo.png"
+                    alt="user icon"
+                  />
+                </div>
+                <div class="flex flex-row items-center mx-3">
+                  <div class="notiUserName font-bold text-lg">UserName</div>
+                  <div class="text-slate-400 text-sm ml-2">2분전</div>
+                </div>
+                <div class="icon ml-auto -mr-3 mt-3 relative inline-block">
+                  <!-- 미트볼 아이콘-->
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="w-6 h-6 pt-1"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M12 6.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 12.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 18.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5Z"
+                    />
+                  </svg>
+                </div>
+              </div>
+              <div class="text-left ml-6 mt-2">댓글내용~~~ 01</div>
+            </div>
+
+            <div class="p-3 border-b border-b-slate-300 pl-10">
+              <div class="postHerder flex flex-row m">
+                <div class="h-14 w-14 overflow-hidden relative">
+                  <img
+                    class="postDetailUserImg object-contain rounded-full"
+                    src="@/assets/logo.png"
+                    alt="user icon"
+                  />
+                </div>
+                <div class="flex flex-row items-center mx-3">
+                  <div class="notiUserName font-bold text-lg">UserName2</div>
+                  <div class="text-slate-400 text-sm ml-2">2분전</div>
+                </div>
+                <div class="icon ml-auto -mr-3 mt-3 relative inline-block">
+                  <!-- 미트볼 아이콘-->
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="w-6 h-6 pt-1"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M12 6.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 12.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 18.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5Z"
+                    />
+                  </svg>
+                </div>
+              </div>
+              <div class="text-left ml-6 mt-2">댓글내용~~~ 02</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import PostDetail from "./post/PostDetail.vue";
+export default {
+  name: "PostModal",
+  components: {
+    PostDetail,
+  },
+  props: {
+    isOpen: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  watch: {
+    isOpen(newValue) {
+      if (newValue) {
+        document.body.classList.add("overflow-hidden");
+      } else {
+        document.body.classList.remove("overflow-hidden");
+      }
+    },
+  },
+  mounted() {
+    if (this.isOpen) {
+      document.body.classList.add("overflow-hidden");
+    }
+  },
+  beforeUnmount() {
+    document.body.classList.remove("overflow-hidden");
+  },
+  methods: {
+    closeModal() {
+      this.$emit("close");
+    },
+  },
+};
+</script>
---
+++ mood-tracker - 복사본/src/views/auth/SignupUsername.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="flex flex-col items-center justify-center h-full p-6">
+    <div class="bg-white rounded-lg p-8 h-[450px] shadow-md w-full max-w-sm">
+      <div class="flex flex-col items-center mb-6">
+        <!-- 로고나 아이콘을 넣을 수 있는 공간 -->
+        <svg
+          class="w-12 h-12 text-gray-500 mb-2"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <!-- SVG content here -->
+        </svg>
+        <span class="text-xl font-semibold">닉네임 설정</span>
+      </div>
+      <form class="flex flex-col space-y-4" @submit.prevent="onNextClick">
+        <div class="flex flex-col">
+          <label for="username" class="text-left mb-2">닉네임</label>
+          <input
+            id="username"
+            type="text"
+            placeholder="닉네임"
+            v-model="username"
+            class="border-2 border-gray-300 p-2 mb-2 rounded-md focus:border-[#64CCC5] focus:outline-none"
+          />
+          <span class="text-sm h-3 text-left text-red-500">{{
+            errors.username
+          }}</span>
+        </div>
+
+        <button
+          type="submit"
+          class="bg-[#64CCC5] text-white p-2 rounded-lg shadow hover:bg-[#3f827e]"
+        >
+          다음
+        </button>
+      </form>
+    </div>
+  </div>
+</template>
+<script>
+import { useForm, useField } from "vee-validate";
+import * as yup from "yup";
+import { useRouter } from "vue-router";
+import { ref, onMounted } from "vue";
+const validationSchema = yup.object().shape({
+  username: yup
+    .string()
+    .required("사용자 이름을 입력해주세요")
+    .min(4, "사용자 이름은 4자 이상이어야 합니다")
+    .matches(
+      /^[가-힣a-z0-9_]+$/,
+      "사용자 이름은 한글, 영어 소문자, 숫자, 밑줄(_)만 가능합니다"
+    )
+    .matches(/^\S*$/, "사용자 이름에 공백이 포함될 수 없습니다"),
+});
+
+export default {
+  name: "SignupPassword",
+  setup() {
+    const router = useRouter();
+    const userInfo = ref({});
+    onMounted(() => {
+      const state = history.state;
+      console.log(state.userInfo);
+      if (state) {
+        userInfo.value = state.userInfo;
+      }
+    });
+
+    const { handleSubmit, errors } = useForm({ validationSchema });
+    const { value: username } = useField("username");
+
+    const onNextClick = handleSubmit((values) => {
+      console.log(values);
+      goToNextStep();
+    });
+
+    const goToNextStep = () => {
+      router.push({
+        name: "SignupBday",
+        state: {
+          userInfo: {
+            userId: userInfo.value.userId,
+            password: userInfo.value.password,
+            userName: username.value,
+          },
+        },
+      });
+    };
+
+    return {
+      username,
+      errors,
+      onNextClick,
+    };
+  },
+};
+</script>
+<style lang=""></style>
