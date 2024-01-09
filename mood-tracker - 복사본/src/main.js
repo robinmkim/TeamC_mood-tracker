4d7a3a3262c92df8c3e6152ee2f5a9b6409b3a3c
Lee-Giyoung <145593169+Lee-Giyoung@users.noreply.github.com>
2024-01-09 14:16:00 +0900

    ✨ FnQ Insert등록

--- mood-tracker - 복사본/src/main.js
+++ mood-tracker - 복사본/src/main.js
@@ -8,16 +8,6 @@ import router from "./router/index";
 const app = createApp(App);
 
 app.config.globalProperties.$axios = axios; //전역변수로 설정 컴포넌트에서 this.$axios 호출할 수 있게 설정
-// 이전 api
 app.config.globalProperties.$serverUrl = "//localhost:8081"; //api server
-// const serverUrl = '//localhost:8081';
-<<<<<<< HEAD
-=======
-
-// 테스트를 위해 추가
-app.config.globalProperties.$serverUrl = "http://192.168.0.50:8083/cteam"; //api server
-const serverUrl = 'http://192.168.0.50:8083/cteam';
-app.provide('$serverUrl', serverUrl);
->>>>>>> 11993f6 (✨ FnQ Insert등록)
 
 app.use(router).mount("#app");
