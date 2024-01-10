d46631ee33f99580b11bea38075d5bb3b9a5530f
Lee-Giyoung <145593169+Lee-Giyoung@users.noreply.github.com>
2024-01-10 10:42:54 +0900

    ✨ admin crud 추가

--- mood-tracker/src/views/admin/AdminPage.vue
+++ mood-tracker/src/views/admin/AdminPage.vue
@@ -375,7 +375,10 @@
                   </li>
                 </ul>
                 >>>>>>> f43a5cb (✅ 물리 Merge
-                완료):mood-tracker/src/views/admin/adminPage.vue
+                완료):mood-tracker/src/views/admin/adminPage.vue =======
+                <FnaList></FnaList>
+                >>>>>>> 68e616b (✨ admin crud
+                추가):mood-tracker/src/views/admin/adminPage.vue
               </div>
               <!-- QnA 게시판 상세 tab -->
               <div v-else-if="currentSubTab === 1">
@@ -522,7 +525,7 @@
     </div>
   </div>
 </template>
-<!-- <li v-for="(item, newIndex) in FnAitems" :key="newIndex"></li> -->
+
 <script>
 // FnAitems 에서 작성자 테스트를 위해 user삭제
 import PostDetailPageVue from "../post/PostDetailPage.vue";
