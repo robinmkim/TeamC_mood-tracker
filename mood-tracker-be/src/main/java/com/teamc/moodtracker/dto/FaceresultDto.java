64b0a35140e6e6648fb1981ec3c442a09377b83d
youngho3313 <youngho3313@gmail.com>
2024-01-10 10:26:05 +0900

    🚧 감정분석-브랜치분리-장고,스프링,DB연결완료-피드백 부분 작업중입니다.

--- mood-tracker-be/src/main/java/com/teamc/moodtracker/dto/FaceresultDto.java
+++ mood-tracker-be/src/main/java/com/teamc/moodtracker/dto/FaceresultDto.java
@@ -4,8 +4,11 @@ import lombok.Getter;
 
 @Getter
 public class FaceresultDto {
-    private int resultId;
-    private String memberId;
-    private String imageFile;
-    private String resultset; // {happy:0.675525, sad:0.231122, ...}
+    private int ar_id; // 분석결과_id
+    private int ar_member_num; // 회원_num
+    private String ar_origin_img; // 원본_이미지 파일이름
+    private String ar_generated_img; // 분석결과_합성_이미지 파일이름
+    private String ar_content; // 분석결과_내용
+    private String ar_content_max; // 분석결과_내용 중 가장 확률이 높은 항목
+    private String ar_analyze_date; // 분석_날짜
 }
--- mood-tracker-be/src/main/java/com/teamc/moodtracker/service/FaceresultService.java
+++ mood-tracker-be/src/main/java/com/teamc/moodtracker/service/FaceresultService.java
@@ -1,7 +1,28 @@
 package com.teamc.moodtracker.service;
 
+import com.teamc.moodtracker.dto.FaceresultDto;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public interface FaceresultService {
+    public void insertFaceResult(FaceresultDto faceresultDto);
+    public List<FaceresultDto> selectList();
+    public List<FaceresultDto> selectMyList(int memberNum);
+    public int selectIdOfMyLastFaceResult(int memberNum);
+    public FaceresultDto selectFaceResultDetail(int ar_id);
+    public void deleteFaceResult(int resultId);
+
+
+//    // 입력
+//    public void createFaceResult(FaceresultDto faceresultDto);
+//
+//    // 조회
+//    public List<FaceresultDto> selectList();
+//    public List<FaceresultDto> selectMyList(String memdberId);
+//
+//    // 삭제
+//    public void deleteFaceResult(int resultId);
+
 }
