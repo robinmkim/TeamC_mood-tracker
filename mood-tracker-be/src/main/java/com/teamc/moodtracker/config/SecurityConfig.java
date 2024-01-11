d35e0c6e7b5bc003cc85b5b5002e5ecd55e64da2
dora2emong <zhj7634@naver.com>
2024-01-11 10:00:11 +0900

    ✨ 채팅 백엔드 구현

--- mood-tracker-be/src/main/java/com/teamc/moodtracker/config/SecurityConfig.java
+++ mood-tracker-be/src/main/java/com/teamc/moodtracker/config/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.annotation.authentication.configurati
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import lombok.RequiredArgsConstructor;
 import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
