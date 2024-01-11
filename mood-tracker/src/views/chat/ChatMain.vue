b80c21da41401a1a60528785c666711231d69016
dora2emong <zhj7634@naver.com>
2024-01-11 10:07:00 +0900

    💄 채팅 통신을 위해 채팅 페이지 수정

--- mood-tracker/src/views/chat/ChatMain.vue
+++ mood-tracker/src/views/chat/ChatMain.vue
@@ -122,8 +122,7 @@
     </div>
   </div>
 </template>
-<<<<<<< HEAD ======= >>>>>>> 37410fb (✨ 채팅 페이지 web socket 관련 메서드
-추가)
+
 <script>
 import { reactive } from "vue";
 import SockJS from "sockjs-client";
@@ -196,6 +195,187 @@ export default {
         top: rect.bottom + window.scrollY + 5,
         left: rect.left + window.scrollX - 50,
       };
+    },
+    data() {
+      return {
+        menuPosition: { top: 0, left: 0 },
+        rooms: [],
+        // 현재 선택한 채팅방의 ID
+        roomId: 1,
+        stompClient: null,
+        // 채팅 입력창에 입력한 메시지
+        message: "",
+        // 현재 로그인한 사용자의 ID
+        memberId: 1,
+        // 현재 채팅방의 메시지
+        // 최초 렌더링 시 서버에서 가져온 메시지 목록으로 초기화
+        messages: [],
+        // 현재 로그인한 사용자가 구독 중인 채팅방 목록
+        // 중복 구독 방지를 위해 roomId 저장
+        subscribedRooms: [],
+        showModal: false,
+        members: [],
+        subscriptionId: null,
+      };
+    },
+    async created() {
+      this.connect();
+      this.loadChatRooms();
+    },
+    methods: {
+      autoExpand(event) {
+        event.target.style.height = "auto";
+        event.target.style.height = event.target.scrollHeight + "px";
+      },
+      toggleMenu(index, event) {
+        // 모든 채팅방의 메뉴 상태를 닫음
+        this.chatList.forEach((chat, i) => {
+          if (i !== index && chat.isMenuOpen) {
+            this.chatList[i] = reactive({
+              ...chat,
+              isMenuOpen: false,
+            });
+          }
+        });
+
+        // 선택한 채팅방의 메뉴 상태를 토글
+        const rect = event.target.getBoundingClientRect();
+        this.chatList[index] = reactive({
+          ...this.chatList[index],
+          menuPosition: {
+            top: rect.bottom + window.scrollY + 5,
+            left: rect.left + window.scrollX - 50,
+          },
+          isMenuOpen: !this.chatList[index].isMenuOpen,
+        });
+
+        // 메뉴 상태 업데이트 후 컴포넌트의 일반 상태를 업데이트
+        this.menuPosition = {
+          top: rect.bottom + window.scrollY + 5,
+          left: rect.left + window.scrollX - 50,
+        };
+      },
+      createChatRoom() {
+        this.getMemberList();
+        this.showModal = true;
+      },
+      getMemberList() {
+        axios
+          .get("http://192.168.0.214:8083/member/list")
+          .then((res) => {
+            this.members = res.data;
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      },
+      connect() {
+        const socket = new SockJS("http://192.168.0.214:8083/ws");
+        this.stompClient = Stomp.over(socket);
+
+        this.stompClient.connect(
+          {},
+          (frame) => {
+            console.log(frame);
+          },
+          (error) => {
+            console.log(error);
+          }
+        );
+      },
+      onerror(error) {
+        console.error("Connection error: " + error);
+      },
+      onMessageReceived(payload) {
+        // 내 메시지와 다른 사람의 메시지 구분을 위해 isMine 값 추가
+        let parseMessage = JSON.parse(payload.body);
+        if (parseMessage.memberId === this.memberId) parseMessage.isMine = true;
+        else parseMessage.isMine = false;
+
+        // 날짜 포맷
+        const hours = parseMessage.sendTime.substring(11, 13);
+        const minutes = parseMessage.sendTime.substring(14, 16);
+        const period = hours >= 12 ? "오후" : "오전";
+        parseMessage.sendTime = `${period} ${hours}:${minutes}`;
+        this.messages.push(parseMessage);
+      },
+      sendMessage() {
+        if (!this.message.trim()) {
+          return;
+        }
+        this.message = this.message.trim();
+        let chatMessage = {
+          roomId: this.roomId,
+          memberId: this.memberId,
+          message: this.message,
+        };
+        if (this.stompClient) {
+          // webstomp-client의 send() 메서드를 사용하여 서버로 메시지 전송
+          // send(destination, body, headers)
+          this.stompClient.send(
+            "/pub/chat/send",
+            JSON.stringify(chatMessage),
+            {}
+          );
+          this.message = "";
+        }
+      },
+      loadChatRooms() {
+        // 현재 로그인한 사용자의 채팅방 목록 조회
+        axios
+          .get(`http://192.168.0.214:8083/rooms/${this.memberId}`)
+          .then((response) => {
+            this.rooms = "";
+            this.rooms = response.data;
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      },
+      // 채팅방 클릭 시 해당 방에 해당하는 채팅 목록을 불러오는 메서드
+      loadMessages(roomId) {
+        // 기존 방 구독 해제
+        // subscribe 시 생성된 subscriptionId를 사용하여 구독 해제
+        this.stompClient.unsubscribe(`${this.subscriptionId}`, {});
+        // this.subscribedRooms = this.subscribedRooms.filter(room => room !== this.roomId)
+
+        //  새로운 방 구독
+        // if(!this.subscribedRooms.includes(roomId)) {
+        //     this.subscribedRooms.push(roomId);
+        this.subscriptionId = this.stompClient.subscribe(
+          `/topic/${roomId}`,
+          this.onMessageReceived
+        ).id;
+        // }
+        this.roomId = roomId;
+        axios
+          .get(`http://192.168.0.214:8083/rooms/${roomId}/messages`)
+          .then((response) => {
+            // 서버에서 roomId에 해당하는 채팅방의 메시지 목록을 가져옴
+            // 가져온 메시지 목록으로 this.messages 초기화
+            this.messages = [];
+            this.messages = response.data;
+
+            // 가져온 메시지들을 내 메시지와 상대 메시지를 구분하기 위해 isMine 값 추가
+            for (let i = 0; i < this.messages.length; i++) {
+              if (this.messages[i].memberId === this.memberId) {
+                this.messages[i].isMine = true;
+              } else {
+                this.messages[i].isMine = false;
+              }
+              // 날짜 포맷
+              const hours = this.messages[i].sendTime.substring(11, 13);
+              const minutes = this.messages[i].sendTime.substring(14, 16);
+              const period = hours >= 12 ? "오후" : "오전";
+
+              this.messages[i].sendTime = `${period} ${hours}:${minutes}`;
+            }
+            // console.log(response.data);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      },
     },
     createChatRoom() {
       this.getMemberList();
