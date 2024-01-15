e397c1b63294b29d3998196a32b8f63ffec5e444
jin-hyun <abyssul1@naver.com>
2024-01-15 12:43:27 +0900

    👔 comment and reply 中

--- mood-tracker-be/src/main/java/com/teamc/moodtracker/dto/Board_LikeDto.java
+++ mood-tracker-be/src/main/java/com/teamc/moodtracker/dto/Board_LikeDto.java
@@ -3,7 +3,7 @@ package com.teamc.moodtracker.dto;
 import lombok.Getter;
 
 @Getter
-public class LikeDto {
+public class Board_LikeDto {
     private String liketype; // 게시글/댓글/대댓글 좋아요인지 구분
     private String memberid; // 좋아요 누른 사람
     private String targetno; // 게시글/댓글 고유번호
