0624146c2e4b58fcd9508837777b97b8f59a15ff
robinmkim <ickhigh@gmail.com>
2024-01-16 17:29:46 +0900

    🐛 충돌 오류 해결

--- mood-tracker/src/views/mypage/FollowTest.vue
+++ mood-tracker/src/views/mypage/FollowTest.vue
@@ -1,48 +1,46 @@
 <template>
-    <div>
-        <button class="mt-10 bg-slate-50" @click="follow(4)">팔로우</button>
-    </div>
+  <div>
+    <button class="mt-10 bg-slate-50" @click="follow(4)">팔로우</button>
+  </div>
 </template>
 
 <script>
-import { jwtDecode } from 'jwt-decode';
-import apiClient from '@/utils/apiClient';
+import { jwtDecode } from "jwt-decode";
+import apiClient from "@/utils/apiClient";
 export default {
-    data() {
-        return {
-            mid: null,
-        }
+  data() {
+    return {
+      mid: null,
+    };
+  },
+  created() {
+    this.mid = this.getLoggedInMember();
+  },
+  methods: {
+    getLoggedInMember() {
+      const token = localStorage.getItem("jwtToken");
+      const decoded = jwtDecode(token);
+      return decoded.m_id;
     },
-    created() {
-        this.mid = this.getLoggedInMember();
+    follow(followedId) {
+      if (this.mid === followedId) {
+        alert("자기 자신을 팔로우할 수 없습니다.");
+        return;
+      }
+      apiClient
+        .post("/follow/test", {
+          followedId: followedId,
+          followerId: this.mid,
+        })
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     },
-    methods: {
-        getLoggedInMember() {
-            const token = localStorage.getItem("jwtToken");
-            const decoded = jwtDecode(token);
-            return decoded.m_id;
-        },
-        follow(followedId) {
-            if (this.mid === followedId) {
-                alert('자기 자신을 팔로우할 수 없습니다.');
-                return;
-            }
-            apiClient.post('http://192.168.0.214:8083/follow/test', {
-                followedId: followedId,
-                followerId: this.mid,
-            })
-            .then((res) => {
-                console.log(res);
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-        }
-    }
+  },
 };
-
 </script>
 
-<style>
-
-</style>
+<style></style>
