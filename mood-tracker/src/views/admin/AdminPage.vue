607dd300f7cddb160d37e90aec657c3f6c922309
Lee-Giyoung <145593169+Lee-Giyoung@users.noreply.github.com>
2024-01-17 20:29:31 +0900

    ✨ admin에 Main Tab구현

--- mood-tracker/src/views/admin/AdminPage.vue
+++ mood-tracker/src/views/admin/AdminPage.vue
@@ -34,8 +34,43 @@
           v-show="currentTab === index"
           role="tabpanel"
         >
-          <!-- 신고게시판 tab -->
+          <!-- Main Tab -->
           <div v-if="currentTab === 0">
+            <AdminMain></AdminMain>
+          </div>
+          <!-- QnA 게시판 Tab -->
+          <div v-if="currentTab === 1">
+            <div class="border m-10">
+              <nav class="flex" role="tablist">
+                <div
+                  v-for="(adminQnATab, adminQnATabIndex) in adminQnATabs"
+                  :key="adminQnATabIndex"
+                  class="text-base h-10 flex-1 flex justify-center items-center border-b-4 py-1.5"
+                  :class="{
+                    'border-[#64CCC5]': currentSubTab === adminQnATabIndex,
+                    'hover:border-[#e0e0e0] border-transparent':
+                      currentSubTab !== adminQnATabIndex,
+                  }"
+                  @click="changeSubTab(adminQnATabIndex)"
+                  role="tab"
+                >
+                  <div class="notiTabName text-left">
+                    {{ adminQnATab.name }}
+                  </div>
+                </div>
+              </nav>
+              <!-- FAQ 게시판 subTab -->
+              <div v-if="currentSubTab === 0">
+                <FaqList></FaqList>
+              </div>
+              <!-- QNA 게시판 상세 subTab -->
+              <div v-else-if="currentSubTab === 1">
+                <QnaList></QnaList>
+              </div>
+            </div>
+          </div>
+          <!-- 신고게시판 tab -->
+          <div v-if="currentTab === 2">
             <div class="border m-10">
               <nav class="flex" role="tablist">
                 <div
@@ -202,38 +237,6 @@
               </div>
             </div>
           </div>
-
-          <!-- QnA 게시판 Tab -->
-          <div v-if="currentTab === 1">
-            <div class="border m-10">
-              <nav class="flex" role="tablist">
-                <div
-                  v-for="(adminQnATab, adminQnATabIndex) in adminQnATabs"
-                  :key="adminQnATabIndex"
-                  class="text-base h-10 flex-1 flex justify-center items-center border-b-4 py-1.5"
-                  :class="{
-                    'border-[#64CCC5]': currentSubTab === adminQnATabIndex,
-                    'hover:border-[#e0e0e0] border-transparent':
-                      currentSubTab !== adminQnATabIndex,
-                  }"
-                  @click="changeSubTab(adminQnATabIndex)"
-                  role="tab"
-                >
-                  <div class="notiTabName text-left">
-                    {{ adminQnATab.name }}
-                  </div>
-                </div>
-              </nav>
-              <!-- FAQ 게시판 subTab -->
-              <div v-if="currentSubTab === 0">
-                <FaqList></FaqList>
-              </div>
-              <!-- QNA 게시판 상세 subTab -->
-              <div v-else-if="currentSubTab === 1">
-                <QnaList></QnaList>
-              </div>
-            </div>
-          </div>
         </div>
       </div>
     </div>
@@ -245,6 +248,7 @@ import PostDetailPageVue from "../post/PostDetailPage.vue";
 import MypageMain from "../mypage/MypageMain.vue";
 import FaqList from "../../components/admin/FaqList.vue";
 import QnaList from "../../components/admin/QnaList.vue";
+import AdminMain from "../../components/admin/AdminMain.vue";
 // import Editor from "@toast-ui/editor";
 // import "@toast-ui/editor/dist/toastui-editor.css";
 
@@ -260,8 +264,9 @@ export default {
       answeringShow: false,
       // 아코디언의 열림/닫힘 상태를 저장하는 배열 추가
       tabs: [
-        { name: "신고게시판", id: "adminComplaint" },
+        { name: "홈", id: "adminMain" },
         { name: "QnA게시판", id: "adminQnA" },
+        { name: "신고게시판", id: "adminComplaint" },
       ],
       adminComplaintTabs: [
         { name: "게시글", id: "adminComplaintPost" },
@@ -271,6 +276,7 @@ export default {
         { name: "Faq", id: "adminQnAPost" },
         { name: "QnA", id: "adminQnAUser" },
       ],
+      adminMainTabs: [{ name: "Mood", id: "adminMainMood" }],
       Postitems: [
         {
           number: 1,
@@ -313,15 +319,8 @@ export default {
       ],
     };
   },
-  components: { PostDetailPageVue, MypageMain, FaqList, QnaList },
+  components: { PostDetailPageVue, MypageMain, FaqList, QnaList, AdminMain },
   methods: {
-    formatRegDate(redate) {
-      const date = new Date(redate);
-      const year = date.getFullYear();
-      const month = String(date.getMonth() + 1).padStart(2, "0");
-      const day = String(date.getDate()).padStart(2, "0");
-      return `${year}-${month}-${day}`;
-    },
     changeTab(index, tabId) {
       this.currentTab = index;
       this.currentSubTab = 0;
