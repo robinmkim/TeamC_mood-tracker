6ac00bdbfbfb48bc0b88ca2c161d30e9a8483b19
youngho3313 <youngho3313@gmail.com>
2024-01-19 10:48:42 +0900

    ✨ 실시간 알림 구현(댓글 작성, 대댓글 작성, 게시물 좋아요) 2

--- mood-tracker-be/src/main/java/com/teamc/moodtracker/controller/JH/JH_CommentController.java
+++ mood-tracker-be/src/main/java/com/teamc/moodtracker/controller/JH/JH_CommentController.java
@@ -3,10 +3,12 @@ package com.teamc.moodtracker.controller.JH;
 import com.teamc.moodtracker.dto.Comment_LikeDto;
 import com.teamc.moodtracker.dto.JH.JH_CommentDto;
 import com.teamc.moodtracker.dto.MemberDto;
+import com.teamc.moodtracker.service.BoardService;
 import com.teamc.moodtracker.service.JH.JH_CommentLikeService;
 import com.teamc.moodtracker.service.JH.JH_CommentService;
 import com.teamc.moodtracker.service.JH.JH_ReplyLikeService;
 import com.teamc.moodtracker.service.JH.JH_ReplyService;
+import com.teamc.moodtracker.service.NotificationService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,29 +31,30 @@ public class JH_CommentController {
 
     @Autowired
     private JH_ReplyService replyService;
+    private NotificationService notificationService; // 윤영호
 
     @GetMapping("/allCommentCount")
     public int allCommentCount(@RequestParam(value = "b_id") int b_id) {
-        List<JH_CommentDto> comments= getCommentList(b_id);
+        List<JH_CommentDto> comments = getCommentList(b_id);
         int replyCount = 0;
         for (JH_CommentDto dto : comments) {
             replyCount += commentService.replyCount(dto.getCm_id());
         }
-        return commentService.commentCount(b_id)+replyCount;
+        return commentService.commentCount(b_id) + replyCount;
     };
 
     @GetMapping("/getCommentList")
     public List<JH_CommentDto> getCommentList(int b_id) {
-      return commentService.getCommentList(b_id);
+        return commentService.getCommentList(b_id);
     };
 
     @GetMapping("/getCommentListDetail")
-    public List<JH_CommentDto> getCommentListDetail(@AuthenticationPrincipal MemberDto memberDto,@RequestParam("b_id") int b_id) {
+    public List<JH_CommentDto> getCommentListDetail(@AuthenticationPrincipal MemberDto memberDto,
+            @RequestParam("b_id") int b_id) {
         System.out.println("getCommentListDetail");
-        System.out.println("b_id::::::::::::::::" +b_id);
+        System.out.println("b_id::::::::::::::::" + b_id);
 
-
-        List<JH_CommentDto> comments=commentService.getCommentListDetail(b_id);
+        List<JH_CommentDto> comments = commentService.getCommentListDetail(b_id);
 
         Comment_LikeDto dto = new Comment_LikeDto();
         dto.setM_id(memberDto.getM_id());
@@ -64,21 +67,22 @@ public class JH_CommentController {
         return comments;
     };
 
-
     @PostMapping("/addComment")
     public int addComment(@AuthenticationPrincipal MemberDto memberDto,
-                          @ModelAttribute JH_CommentDto dto) {
+            @ModelAttribute JH_CommentDto dto) {
         System.out.println("addComment!!!!!!!!");
-//        JH_CommentDto dto = new JH_CommentDto();
+        // JH_CommentDto dto = new JH_CommentDto();
         dto.setM_id(memberDto.getM_id());
-//        dto.setB_id(b_id);
-//        dto.setCm_content(cm_content);
-        System.out.println("m_id: "+dto.getM_id());
-        System.out.println("b_id: "+dto.getB_id());
+        // dto.setB_id(b_id);
+        // dto.setCm_content(cm_content);
+        System.out.println("m_id: " + dto.getM_id());
+        System.out.println("b_id: " + dto.getB_id());
         System.out.println(dto.getCm_content());
         System.out.println(dto);
 
         commentService.addComment(dto);
+
+        notificationService.addComment_SaveNotificationAndSendAlert(dto); // 알림 // 윤영호
         return 1;
     };
 
@@ -89,23 +93,25 @@ public class JH_CommentController {
 
         List<Integer> replyList = replyService.getRe_idList(cm_id);
         for (int reply : replyList) {
-            System.out.println("re_id: "+ reply);
+            System.out.println("re_id: " + reply);
             replyService.delReply(reply);
         }
-//        if (likeService.commentLikeCount(cm_id) > 0) {
-//             likeService.delCommentLike(cm_id);
-//        }
+        // if (likeService.commentLikeCount(cm_id) > 0) {
+        // likeService.delCommentLike(cm_id);
+        // }
         commentService.delComment(cm_id);
     }
 
     @GetMapping("/getCm_idList")
-    public List<Integer> getCm_idList(@RequestParam("b_id") int b_id){
+    public List<Integer> getCm_idList(@RequestParam("b_id") int b_id) {
 
         return commentService.getCm_idList(b_id);
     }
+
     @Transactional
     @GetMapping("/getCommentDetail")
-    public JH_CommentDto getCommentDetail(@AuthenticationPrincipal MemberDto memberDto, @RequestParam("cm_id") int cm_id) {
+    public JH_CommentDto getCommentDetail(@AuthenticationPrincipal MemberDto memberDto,
+            @RequestParam("cm_id") int cm_id) {
         JH_CommentDto comment = commentService.getCommentDetail(cm_id);
         Comment_LikeDto dto = new Comment_LikeDto();
         dto.setM_id(memberDto.getM_id());
