12d4df5b59c75cbbd2f3163ba95b428bbc46c250
Lee-Giyoung <145593169+Lee-Giyoung@users.noreply.github.com>
2024-01-22 11:57:16 +0900

    ✨ 관리자 게시글, 신고 게시글 구현

--- mood-tracker-be/src/main/java/com/teamc/moodtracker/service/ReportService.java
+++ mood-tracker-be/src/main/java/com/teamc/moodtracker/service/ReportService.java
@@ -1,7 +1,46 @@
 package com.teamc.moodtracker.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.teamc.moodtracker.dao.ReportDao;
+import com.teamc.moodtracker.dto.ReportDto;
+
+import jakarta.transaction.Transactional;
+
 @Service
-public interface ReportService {
+public class ReportService {
+    @Autowired
+    private ReportDao reportDao;
+
+    // 게시글
+    public List<ReportDto> reportList() {
+        return reportDao.reportList();
+    };
+
+    public ReportDto reportDetail(int report_id) {
+        return reportDao.reportDetail(report_id);
+    }
+
+    // 댓글
+    public List<ReportDto> reportCommentList() {
+        return reportDao.reportCommentList();
+    }
+
+    public ReportDto reportCommentDetail(int comment_id) {
+        return reportDao.reportCommentDetail(comment_id);
+    }
+
+    @Transactional
+    public void addReport(ReportDto dto) {
+        reportDao.addReport(dto);
+    }
+    // public List<Integer> reportList(int lastRowNum) {
+    // Map<String, Object> params = new HashMap<>();
+    // params.put("lastRowNum", lastRowNum);
+    // return ReportDao.reportList(params);
+    // }
+
 }
--- mood-tracker/src/views/question/questionPage.vue
+++ mood-tracker/src/views/question/questionPage.vue
@@ -5,20 +5,23 @@
         <span
           class="pl-3 pr-10 inline-block w-36 border-b-2 mb-4 placeholder: border-b-slate-300"
         >
-          문의하기
+          질문하기
         </span>
         <input
           type="text"
           class="rounded-lg h-10 bg-slate-50 border placeholder-slate-400 border-slate-200 focus:outline-slate-400 mb-2"
-          placeholder=" 문의 제목을 입력해 주십시오."
+          placeholder=" 질문 제목을 입력해 주십시오."
+          v-model="qTitle"
         />
         <textarea
           class="bg-slate-50 rounded-lg border border-slate-200 focus:outline-slate-400 w-full h-80 resize-none"
-          placeholder=" 문의 내용을 입력해 주십시오."
+          placeholder=" 질문 내용을 입력해 주십시오."
+          v-model="qContent"
         ></textarea>
         <div class="flex justify-center items-center mt-4">
           <div
             class="rounded-full bg-[#ffede6] h-10 w-28 flex justify-center items-center"
+            @click="questionInsert()"
           >
             등록하기
           </div>
@@ -29,7 +32,51 @@
 </template>
 
 <script>
+import apiClient from "@/utils/apiClient";
 export default {
+  props: ["resultId"],
+  data() {
+    return {
+      // memberHandle: "",
+      q_title: "",
+      q_content: "",
+    };
+  },
+  name: "PostWrite",
+  mounted() {
+    // // userName을 가져옵니다.
+    // apiClient.get("/member/userInfo/memberHandle").then((res) => {
+    //   console.log("memberHandle = ", res.data);
+    //   this.memberHandle = res.data;
+    // });
+  },
+  methods: {
+    adjustHeight(e) {
+      // textarea높이 자동 조절
+      const element = e.target;
+      element.style.height = "auto";
+      element.style.height = element.scrollHeight + "px";
+    },
+    async questionInsert() {
+      const apiUrl = "/question/add";
+      const requestData = {
+        q_title: this.qTitle,
+        q_content: this.qContent,
+      };
+
+      try {
+        const response = await apiClient.post(apiUrl, requestData);
+        console.log(response.data);
+        alert("Question 등록 성공");
+        this.insertOpenSuccessPopup();
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    insertOpenSuccessPopup() {
+      this.$router.go(0);
+    },
+  },
   components: {},
 };
 </script>
