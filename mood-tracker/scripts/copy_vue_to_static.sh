04473f436c860ca818c5af401954b4cc49438d56
robinmkim <ickhigh@gmail.com>
2024-01-24 21:50:00 +0900

    🐛 script 실행 오류

--- mood-tracker/scripts/copy_vue_to_static.sh
+++ mood-tracker/scripts/copy_vue_to_static.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 # Vue.js 빌드 결과물을 Spring Boot의 static 디렉토리로 복사
-cp -R /tmp/vue-dist/* /var/app/mood-tracker-be/src/main/resources/static/
+#!/bin/bash
+cp -R /home/ubuntu/app/mood-tracker/dist/* /home/ubuntu/app/src/main/resources/static/
+
