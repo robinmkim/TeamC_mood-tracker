433272818b09cce6fa6c3127efcd58b4491fa0ab
robinmkim <ickhigh@gmail.com>
2024-01-24 11:13:55 +0900

    🧪 CircleCI 테스트

--- mood-tracker/vue.config.js
+++ mood-tracker/vue.config.js
@@ -1,4 +1,11 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  publicPath: "/",
+  transpileDependencies: true,
+  outputDir: "../mood-tracker-be/src/main/resources/static",
+  devServer: {
+    port: 3000,
+    proxy: "http://54.180.102.205",
+  },
+});
