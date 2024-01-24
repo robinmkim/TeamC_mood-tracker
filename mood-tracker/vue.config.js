2b960f3cad8518b853d662e0fafe14d5eda45b1f
robinmkim <ickhigh@gmail.com>
2024-01-25 05:15:20 +0900

    build 경로 수정

--- mood-tracker/vue.config.js
+++ mood-tracker/vue.config.js
@@ -1,10 +1,4 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  publicPath: "/",
   transpileDependencies: true,
-  outputDir: "../mood-tracker-be/src/main/resources/static",
-  devServer: {
-    port: 3000,
-    proxy: "http://54.180.102.205",
-  },
 });
