1a6db22bbac72b05fb717d22d687d27c8a821bd5
robinmkim <ickhigh@gmail.com>
2024-01-24 22:01:39 +0900

    🐛 test

--- mood-tracker-be/scripts/deploy.sh
+++ mood-tracker-be/scripts/deploy.sh
@@ -2,4 +2,4 @@
 
 cd /home/ubuntu/deploy/build/libs
 
-nohup java -jar jlpt-0.0.1-SNAPSHOT.jar > /home/ubuntu/jlpt/nohup.out 2>&1 &
+nohup java -jar mood-tracker-be-0.0.1-SNAPSHOT.jar > /home/ubuntu/app/nohup.out 2>&1 &
