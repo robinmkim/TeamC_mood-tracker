ecac637907af10f1d18501104a5335348d414cff
robinmkim <ickhigh@gmail.com>
2024-01-25 04:21:24 +0900

    🔧 kill 부분 script 수정

--- mood-tracker-be/scripts/stop.sh
+++ mood-tracker-be/scripts/stop.sh
@@ -7,14 +7,13 @@ DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
 TIME_NOW=$(date +%c)
 
-# 현재 구동 중인 애플리케이션 pid 확인
+
 CURRENT_PID=$(pgrep -f $JAR_FILE)
 
-pkill -f "$CURRENT_PID"
-
 # 프로세스가 켜져 있으면 종료
-if [ $? -eq 0 ]; then
-  echo "$TIME_NOW > 실행중인 애플리케이션을 종료했습니다" >> $DEPLOY_LOG
+if [ -z $CURRENT_PID ]; then
+  echo "$TIME_NOW > 현재 실행중인 애플리케이션이 없습니다" >> $DEPLOY_LOG
 else
-  echo "$TIME_NOW > 실행중인 애플리케이션이 없습니다" >> $DEPLOY_LOG
+  echo "$TIME_NOW > 실행중인 $CURRENT_PID 애플리케이션 종료 " >> $DEPLOY_LOG
+  sudo kill -15 $CURRENT_PID
 fi
