fcbc26ffb7d186ed101332d3fac1b36cfdacbecb
robinmkim <ickhigh@gmail.com>
2024-01-25 00:34:19 +0900

    🔧 script 내용 수정

--- mood-tracker/copy_vue_to_static.sh
+++ mood-tracker/copy_vue_to_static.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Vue.js 빌드 결과물을 Spring Boot의 static 디렉토리로 복사
 #!/bin/bash
-cp -R /home/ubuntu/app/mood-tracker/dist/* /home/ubuntu/app/src/main/resources/static/
+cp -R /home/ubuntu/app/vue-project/dist/* /home/ubuntu/app/src/main/resources/static/
 
