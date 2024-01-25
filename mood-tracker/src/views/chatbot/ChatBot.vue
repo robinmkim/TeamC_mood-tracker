c41c43a7d5d4c73229a12068d1082d0be4d63806
robinmkim <ickhigh@gmail.com>
2024-01-26 03:29:00 +0900

    💄 챗봇 UI 추가

--- mood-tracker/src/views/chatbot/ChatBot.vue
+++ mood-tracker/src/views/chatbot/ChatBot.vue
@@ -9,25 +9,36 @@
         <div
           v-for="(message, index) in messages"
           :key="index"
-          class="flex justify-center"
+          class="flex justify-center overflow-auto"
         >
           <div
             v-if="message.isMine"
-            class="flex flex-col justify-center items-center w-2/3 mb-2"
+            class="flex flex-col items-center w-2/3 mb-2"
           >
-            <div class="flex justify-end w-full">
-              <span class="text-xs text-gray-400">{{ message.message }}</span>
+            <div class="flex flex-row ml-auto mt-3 break-all text-right">
+              <div class="rounded-lg bg-gray-300 p-2 max-w-52">
+                <span>{{ message.message }}</span>
+              </div>
             </div>
           </div>
           <div
             v-else
             class="flex flex-col justify-center items-center w-2/3 mb-2"
           >
-            <div class="flex justify-start w-full">
-              <span class="text-xs text-gray-400">{{ message.message }}</span>
-            </div>
-            <div class="flex justify-start w-full">
-              <a class="text-xs text-gray-400">{{ message.musicLink }}</a>
+            <div class="flex flex-col mr-auto mt-3 break-all text-left">
+              <div class="rounded-lg bg-gray-300 p-2 w-64 text-wrap">
+                <span v-html="formatMessage(message.message)"></span>
+              </div>
+              <div class="rounded-lg bg-gray-300 mt-3 p-2 w-52 text-wrap">
+                아래 제목을 누르면 해당 노래의 YouTube 영상으로 이동합니다<br />
+                <a
+                  v-for="(item, index) in message.musicTitle"
+                  :href="message.musicLink[index]"
+                  target="_blank"
+                  :key="index"
+                  >{{ index }}. {{ item }}<br
+                /></a>
+              </div>
             </div>
           </div>
         </div>
@@ -39,7 +50,11 @@
           class="w-2/3 border rounded-md mr-2"
           type="text"
         />
-        <button @click="sendPrompt" class="border rounded-xl px-2 py-1">
+        <button
+          @click="sendPrompt"
+          :disabled="isLoading"
+          class="border rounded-xl px-2 py-1"
+        >
           전송
         </button>
       </div>
@@ -53,24 +68,22 @@ export default {
   name: "ChatBot",
   data() {
     return {
-      isLoding: true,
+      isLoding: false,
       prompt: "",
-      messages: [
-        {
-          isMine: true, // 또는 false
-          message: "",
-          musicLink: "",
-        },
-      ],
+      messages: [],
     };
   },
   methods: {
+    formatMessage(msg) {
+      return msg.replace(/(\n)/g, "<br>");
+    },
     sendPrompt() {
       this.messages.push({
         isMine: true,
         message: this.prompt,
         musicLink: "",
       });
+      this.isLoading = true;
       axios
         .post("http://127.0.0.1:8000/recommend-songs/", {
           prompt: this.prompt,
@@ -79,9 +92,15 @@ export default {
           console.log(res.data);
           this.messages.push({
             isMine: false,
-            message: res.data.result,
+            message: res.data.result.replace(/(\d\.)/g, "\n$1"),
+            musicTitle: res.data.song_titles,
             musicLink: res.data.song_urls,
           });
+          this.isLoading = false;
+        })
+        .catch((err) => {
+          console.log(err);
+          this.isLoading = false;
         });
       this.prompt = "";
     },
