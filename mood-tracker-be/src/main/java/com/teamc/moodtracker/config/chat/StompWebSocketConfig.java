dfad25193eab2320b37582128ab272750d16fc73
dora2emong <zhj7634@naver.com>
2024-01-26 14:56:30 +0900

    ⚡️ WebStomp allowedOrgins 추가

--- mood-tracker-be/src/main/java/com/teamc/moodtracker/config/chat/StompWebSocketConfig.java
+++ mood-tracker-be/src/main/java/com/teamc/moodtracker/config/chat/StompWebSocketConfig.java
@@ -6,26 +6,22 @@ import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBr
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
-
 @Configuration
 @EnableWebSocketMessageBroker
 public class StompWebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("http://192.168.0.214:8081", "http://localhost:8081", "http://192.168.0.43:8081") // CORS 허용
-                .withSockJS(); // SocketJS 연결 설정
+                .setAllowedOrigins(
+                        "http://192.168.0.214:8081", "http://localhost:8081",
+                        "http://192.168.0.43:8081", "http://192.168.0.84:8081")
+                .withSockJS();
     }
 
-    // STOMP에서 사용하는 메시지 브로커 설정
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        // vue에서 메시지를 보내는 주소
         config.setApplicationDestinationPrefixes("/pub");
-
-        // 서버가 메세지를 보내는 주소
-        config.enableSimpleBroker("/topic", "/queue");
+        config.enableSimpleBroker("/topic");
     }
 }
